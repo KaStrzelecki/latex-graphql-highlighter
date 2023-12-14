20f9733f4975fdfde01fc6f06c17c3183f3e3ac4
KaStrzelecki <ka.strzelecki@wp.pl>
2023-12-14 13:49:05 +0100

    Update gql.py
    Modify rule to identify members with parameters as text (not as type)

--- gql.py
+++ gql.py
@@ -32,7 +32,7 @@ class GraphqlLexer(RegexLexer):
             (r'(-?0|-?[1-9][0-9]*)(\.[0-9]+[eE][+-]?[0-9]+|\.[0-9]+|[eE][+-]?[0-9]+)', Number.Float),
             (r'(-?0|-?[1-9][0-9]*)', Number.Integer),
             (r'\$+[_A-Za-z][_0-9A-Za-z]*', Name.Variable),
-            (r'[_A-Za-z][_0-9A-Za-z]+\s?:', Text),
+            (r'[_A-Za-z][_0-9A-Za-z]+\s?(\(|:)', Text),
             (r'(type|query|interface|fragment|mutation|extend|input|implements|directive|@[a-z]+|on\s+\w+|true|false|null)\b', Keyword.Type),
             (r'(\:\s*\w+)\b', Keyword.Type),
             (r'[!$():=@\[\]{|}]+?', Punctuation),
